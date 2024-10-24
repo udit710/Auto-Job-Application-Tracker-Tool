ff4a8c7190ff03d1c96e428e67344b656a185180
udit710 <s3933905@student.rmit.edu.au>
2024-10-24 17:45:47 +1100

    Implemented automatic sheet updating

---
+++ ajat-backend/update_sheet.py
@@ -0,0 +1,39 @@
+
+import ast
+import os
+import gspread
+from google.oauth2.service_account import Credentials
+import pandas as pd
+import datetime
+
+def update_sheet(genData, link):
+
+    # Define the scope
+    scopes = ['https://www.googleapis.com/auth/spreadsheets']
+
+    # Set up credentials
+    creds = Credentials.from_service_account_file('credentials.json', scopes=scopes)
+    client = gspread.authorize(creds)
+
+    # Open the Google Sheet
+    sheet = client.open_by_key(os.getenv('SHEET_ID'))
+
+    # process the data
+    genData = ast.literal_eval(genData)
+    df = pd.DataFrame(sheet.worksheet("Sheet3").get_all_records())
+    data = {
+        "No":len(df)+1,
+        "Role":genData["Role"],
+        "Company":genData["Company"],
+        "Skills":genData["Skills"],
+        "Platform":genData["Platform"],
+        "Link":link,
+        "Applied": datetime.datetime.now().strftime("%d/%m/%y"),
+        "Follow":"", 
+        "Response?":""
+        }
+
+    df = pd.concat([df, pd.DataFrame([data])], ignore_index=True)
+
+    # Update the Google Sheet
+    sheet.worksheet("Sheet3").update([df.columns.values.tolist()] + df.values.tolist())
