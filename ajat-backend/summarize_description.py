b24d9f291e4ce109a42428c1ee7c6b5a5dc6d01f
udit710 <s3933905@student.rmit.edu.au>
2024-10-24 17:44:40 +1100

    Implemented data processing part

---
+++ ajat-backend/summarize_description.py
@@ -0,0 +1,23 @@
+import os
+import google.generativeai as genai
+
+def summarize_description(description):
+    
+    # get the instructions to pass to the model
+    instructions = os.getenv("INSTRUCTIONS")
+    
+    # Initialize the model
+    genai.configure(api_key=os.getenv("GEN_API_KEY"))
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    
+    # Use the model to get essential information from the job description
+    response = model.generate_content(instructions+description)
+    
+    # with open("job_description.txt", "w") as file:
+    #     file.write(response.candidates[0].content.parts[0].text)
+    
+    # print(response.candidates[0].content.parts[0].text)
+        
+    # file.close()
+    
+    return response.candidates[0].content.parts[0].text
